c4effbaf02798e14737a837a41508435824625b3
pcegg <pcegg2002@hotmail.com>
2024-10-01 21:16:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, PostbackEvent, MemberJoinedEvent
 import requests
 import traceback
 
